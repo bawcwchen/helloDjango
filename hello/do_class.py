85d105ff60fadb69311ac814f0b2e1536981c268
bawcwchen <bawcwchen@163.com>
2016-08-25 13:44:19 +0800

    init

---
+++ hello/do_class.py
@@ -0,0 +1,18 @@
+"""
+  class测试,object,self都是系统变量，不能变
+"""
+
+
+class User(object):  # object是一个class
+    def __init__(self, name, age):  # __init__相当于是构造函数
+        self.name = name
+        self.age = age
+
+    def show(self):
+        print('%s:%s' % (self.name, self.age))
+
+
+# 用来测试，如果被其他模块引用，则自动失效
+if __name__ == '__main__':
+    user1 = User("chenJun", 20)
+    user1.show()
