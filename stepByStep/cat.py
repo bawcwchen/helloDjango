a371d2f2feb175fcbcd7b422dff601dff4a85b75
wb-cj189958 <wb-cj189958@alibaba-inc.com>
2016-08-26 17:22:13 +0800

    添加代言

--- stepByStep/cat.py
+++ stepByStep/cat.py
@@ -4,7 +4,7 @@ from stepByStep.animal import Animal
 
 
 class Cat(Animal):
-    __slots__ = ('name', 'age')  # 用tuple定义允许绑定的属性名称
+    __slots__ = ('_name', '_age')  # 用tuple定义允许绑定的属性名称
 
     def __init__(self, name, age):
         super().__init__(name)
@@ -12,6 +12,20 @@ class Cat(Animal):
 
     def sing(self):
         print("%s is sing" % self._name)
+
+    @property
+    def age(self):  # getter
+        return self._age
+
+    @age.setter
+    def age(self, value):  # setter
+        if not isinstance(value, int):
+            raise ValueError("score must be int")
+
+        if value < 0 or value > 100:
+            raise ValueError("score must be 0~100")
+
+        self._age = value
 
 
 if __name__ == '__main__':
