b9d2e5db1361a626f040dc89a1bb60c7b758aa9d
wb-cj189958 <wb-cj189958@alibaba-inc.com>
2016-08-26 15:28:40 +0800

    添加__slots__限制类的属性

--- stepByStep/cat.py
+++ stepByStep/cat.py
@@ -4,6 +4,8 @@ from stepByStep.animal import Animal
 
 
 class Cat(Animal):
+    __slots__ = ('name', 'age')  # 用tuple定义允许绑定的属性名称
+
     def __init__(self, name, age):
         super().__init__(name)
         self._age = age
