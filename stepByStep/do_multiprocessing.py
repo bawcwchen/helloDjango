28daf21b2c712b7da8d270014625af042a67c5a5
chenjun <1298486594@qq.com>
2016-08-28 20:16:35 +0800

    多线程，进程学习

---
+++ stepByStep/do_multiprocessing.py
@@ -0,0 +1,20 @@
+"""
+多进程
+"""
+
+from multiprocessing import Process
+import os
+
+
+# 子进程要执行的代码
+def run_proc(name):
+    print('Run child process %s (%s)...' % (name, os.getpid()))
+
+
+if __name__ == '__main__':
+    print('Parent process %s.' % os.getpid())
+    p = Process(target=run_proc, args=('test',))
+    print('Child process will start.')
+    p.start()
+    p.join()  # 等待p进行执行完
+    print('Child process end.')
